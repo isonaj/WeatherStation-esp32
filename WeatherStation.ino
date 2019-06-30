8e17e6b31b590913d6691fdb28a63f733d7a87dc
Anthony <isonaj@gmail.com>
2019-06-30 21:37:35 +1000

    Added Blynk and Deep Sleep

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -1,40 +1,50 @@
-/* 
- * ESP32 NodeMCU DHT11 - Humidity Temperature Sensor Example
- * https://circuits4you.com
- * 
- * References
- * https://circuits4you.com/2017/12/31/nodemcu-pinout/
- * 
- */
- 
 #include "DHTesp.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 
-#define DHTpin 5    //D5 of ESP32 DevKit
+#define DHTpin 5
+#define TIME_TO_SLEEP 15
+#define uS_TO_MIN_FACTOR 60000000
+
+char auth[] = "MYAUTHKEY";
+char ssid[] = "MYHOMESSID";
+char pass[] = "MYHOMEPASSWORD";
 
 DHTesp dht;
+BlynkTimer timer;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("Status\tHumidity (%)\tTemp (C)\tHeatIndex (C)");
+  delay(1000); 
+  Serial.println("Starting up");
   
-  dht.setup(DHTpin, DHTesp::DHT11); //for DHT11 Connect DHT sensor to GPIO 17
+  dht.setup(DHTpin, DHTesp::DHT11);
+  Blynk.begin(auth, ssid, pass);
 }
 
 void loop()
 {
-  //delay(dht.getMinimumSamplingPeriod());
-  delay(5000);
+  Serial.println("Sending data to Blynk");
+  getData();
+  Blynk.run();
   
+  delay(1000);
+  
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_MIN_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Minutes");
+
+  //Go to sleep now
+  esp_deep_sleep_start();
+}
+
+void getData() {
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
-
-  Serial.print(dht.getStatusString());
-  Serial.print("\t");
-  Serial.print(humidity, 1);
-  Serial.print("\t\t");
-  Serial.print(temperature, 1);
-  Serial.print("\t\t");
-  Serial.println(dht.computeHeatIndex(temperature, humidity, false), 1);
+  float heatIndex = dht.computeHeatIndex(temperature, humidity, false);
+  
+  Blynk.virtualWrite(V1, humidity);
+  Blynk.virtualWrite(V2, temperature);
+  Blynk.virtualWrite(V3, heatIndex);
 }
